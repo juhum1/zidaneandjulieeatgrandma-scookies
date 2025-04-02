0a47de8e7ce2be9477222bac049506ec66874055
zidanekarim <zkarim7676@gmail.com>
2025-04-02 15:36:29 -0400

    removed c files

--- towers.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void attack (struct Tower* towers, struct Enemy enemy) {
-	//tower based on xposition
-	if (enemy.yPosition <= towers[0].range) {
-		enemy.currentHealth -= towers[0].damage;
-	}
-}
