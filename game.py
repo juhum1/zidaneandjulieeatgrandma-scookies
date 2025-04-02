7914e3558dbcfbdabb8f0eb398d289b1b04b0aa3
Zidane Karim <zkarim7676@gmail.com>
2025-04-02 16:18:17 -0400

    created game file

--- game.py
+++ game.py
@@ -1,3 +1,17 @@
 import pygame
 import enemies
 
+pygame.init()
+size = width, height = 1280, 720
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Game")
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    fps = clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
