3bbbc2f9fe4a8cdcd7a08e621da3a7cb2071f77e
Zidane Karim <zkarim7676@gmail.com>
2025-05-01 17:03:42 -0400

    julie changes

--- towers.py
+++ towers.py
@@ -145,7 +145,6 @@ class Slowing (Tower):
         self.price = 200
         self.sprite_surface = scale_image(pygame.image.load(self.sprite).convert_alpha())
 
-<<<<<<< HEAD
     slow_factor = 0.5   
     slow_duration = 2000 
     def shoot(self, enemy, board, i, j, k):
@@ -163,10 +162,6 @@ class Slowing (Tower):
             board.projectiles.append(new_proj)
             self.lastAttackTime = pygame.time.get_ticks()
 
-=======
-        
-    
->>>>>>> e846b5901af1d1528c86891582b5ed9f22f6f4ad
     @staticmethod
     def get_price():
         return 200
